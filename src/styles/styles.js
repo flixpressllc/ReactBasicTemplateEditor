539d13a91b7898e6d0a924570312c77325ee197e
Don Denton <don@happycollision.com>
2016-04-15 15:35:48 -0500

    Add `m` function
    See comment in file for details

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -20,10 +20,22 @@ var styles = {
     backgroundSize: 'contain'
   },
   messages: {
+    backgroundColor: 'green',
     error: {
       color: 'red'
     }
   }
 };
 
-export {styleVars, styles};
+// See slide 32 and beyond at https://speakerdeck.com/vjeux/react-css-in-js
+var m = function () {
+  var res = {};
+  for (var i=0; i < arguments.length; ++i) {
+    if (arguments[i]) {
+      Object.assign(res, arguments[i]);
+    }
+  }
+  return res;
+};
+
+export {styleVars, styles, m};
