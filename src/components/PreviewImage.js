2120b5095f7ef8150f32f9b286c6ee50bbc37953
Don Denton <don@happycollision.com>
2016-04-21 13:55:03 -0500

    PreviewImage: better feedback on missing images

--- src/components/PreviewImage.js
+++ src/components/PreviewImage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {clone} from './imports';
 import Modal from 'react-modal';
+import cx from 'classnames';
+import {m} from '../styles/styles';
 
 const urlPartial = '/templates/images/';
 
@@ -13,19 +15,38 @@ export default React.createClass({
     return {
       image: image,
       style: {backgroundSize: 'contain'},
-      modalIsOpen: false
+      modalIsOpen: false,
+      missing: false
     };
   },
   
   componentWillReceiveProps: function(newProps) {
     if (newProps.image != this.props.image && newProps.image !== '') {
+      if (this.currentCheck) this.currentCheck.abort();
       var style = clone(this.state.style);
       style.backgroundImage = `url("${urlPartial + newProps.image}")`;
-      this.setState({style: style, image: urlPartial + newProps.image});
+      this.setState({
+        style: style,
+        image: urlPartial + newProps.image,
+        missing: false
+      });
+      this.currentCheck = $.ajax({
+        url: urlPartial + newProps.image,
+        type: 'HEAD'
+      }).always((obj1, status, obj2)=>{
+        if (status === 'abort') return;
+        let xhr = status === 'success' ? obj2 : obj1;
+        if (xhr.getResponseHeader('Content-Type').indexOf('image') === -1) {
+          this.setState({
+            missing: true
+          })
+        }
+      });
     }
   },
   
   openModal: function () {
+    if (this.state.missing) return;
     this.setState({modalIsOpen: true})
   },
   
@@ -35,13 +56,17 @@ export default React.createClass({
 
   
   render: function(){
+    var message = 'click to enlarge';
+    if (this.state.missing === true) {
+      message = 'Preview unavailable. Continue editing.';
+    }
     if (this.props.image === '') {
       return (<div className="preview-image-component component"></div>)
     }
     return (
       <div className="preview-image-component component">
-        <div className="preview-image" style={this.state.style} onClick={this.openModal}>
-          <span>Click to enlarge</span>
+        <div className={cx('preview-image',{'missing': this.state.missing})} style={m({cursor: 'default'},this.state.style)} onClick={this.openModal}>
+          <span>{message}</span>
         </div>
         <Modal
           ref="modal"
