58083d65e66ebfa6c1aa68c03f6f1dd281905e54
Don Denton <don@happycollision.com>
2016-04-25 14:06:28 -0500

    set better ENV for dev

--- cfg/flix.js
+++ cfg/flix.js
@@ -15,9 +15,6 @@ let config = Object.assign({}, baseConfig, {
   devtool: 'sourcemap',
   plugins: [
     new webpack.optimize.DedupePlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"'
-    }),
     new BowerWebpackPlugin({
       searchResolveModulesDirectories: false
     }),
