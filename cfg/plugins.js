35ec43ade12b7ecf2a127de5b10bdc410d83a0d2
Don Denton <don@happycollision.com>
2016-10-21 20:19:33 -0500

    add dist min to new config flow

--- cfg/plugins.js
+++ cfg/plugins.js
@@ -28,5 +28,20 @@ module.exports = {
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
     new webpack.NoErrorsPlugin()
+  ],
+  
+  minPlugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
+    }),
+    new BowerWebpackPlugin({
+      searchResolveModulesDirectories: false
+    }),
+    new webpack.optimize.UglifyJsPlugin({minimize: true, include: /\.min\.js$/}),
+    new ExtractTextPlugin('editor.min.css', {allChunks: true}),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new webpack.NoErrorsPlugin()
   ]
 };
