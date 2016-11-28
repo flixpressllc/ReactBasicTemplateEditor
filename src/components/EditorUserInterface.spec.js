6057f7db73a69a471c438bb3df2f7717125437d9
Don Denton <don@happycollision.com>
2016-11-28 17:19:59 -0600

    Add jest testing and really long test of main component

---
+++ src/components/EditorUserInterface.spec.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import EditorUserInterface from './EditorUserInterface';
+
+it('renders without crashing', () => {
+  let settings = {
+    uiSettingsJsonUrl: `/templates/Template79.json`,
+    userSettingsData: {
+      templateType: "text",
+      username: 'DonDenton',
+      templateId: 79,
+      minutesRemainingInContract: 170.2819,
+      minimumTemplateDuration: 0.1667,
+      mode: 'Add',
+      previewVideoUrl: '',
+      isChargePerOrder: 'False',
+      renderCost: 8
+    }
+  };
+  
+  shallow(<EditorUserInterface uiSettingsJsonUrl={ settings.uiSettingsJsonUrl } userSettingsData={ settings.userSettingsData }/>);
+});
+
