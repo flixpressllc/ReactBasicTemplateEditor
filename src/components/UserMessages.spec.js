a9423ffb7ddba074e5ac5c28520e590b23f2181f
Don Denton <don@happycollision.com>
2016-12-01 11:35:27 -0600

    UserMessages spec added

---
+++ src/components/UserMessages.spec.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import UserMessages from './UserMessages';
+
+describe('UserMessages', () => {
+  it('displays a plain text message', () => {
+    const messages = [{message:'a message for you'}];
+    const component = mount(<UserMessages messages={ messages }/>);
+
+    expect(component.text()).toContain('a message for you');
+  })
+
+  it('displays messages in raw html', () => {
+    const messages = [{message:'a mess<span class="disruption"></span>age for you', htmlSafe: true}];
+    const component = mount(<UserMessages messages={ messages }/>);
+
+    expect(component.find('Message').text()).toContain('a message for you');
+  });
+
+  it('allows classes to be set for messages', () => {
+    const messages = [{message:'a message for you', type:'biggest-message'}];
+    const component = mount(<UserMessages messages={ messages }/>);
+
+    expect(component.find('.biggest-message').length).toEqual(1);
+  });
+})
