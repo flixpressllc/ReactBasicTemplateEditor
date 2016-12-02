6789765a7ea3a2f854354ffd581b09ec9c9b1184
Don Denton <don@happycollision.com>
2016-12-02 13:55:02 -0600

    complete specs for UserMessages

--- src/components/UserMessages.spec.js
+++ src/components/UserMessages.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount, render } from 'enzyme';
 import UserMessages from './UserMessages';
 
 describe('UserMessages', () => {
@@ -8,7 +8,29 @@ describe('UserMessages', () => {
     const component = mount(<UserMessages messages={ messages }/>);
 
     expect(component.text()).toContain('a message for you');
-  })
+  });
+
+  it('allows types to be set for messages', () => {
+    const messages = [{message:'a message for you', type:'biggest-message'}];
+    const component = mount(<UserMessages messages={ messages }/>);
+
+    expect(component.find('.biggest-message').length).toEqual(1);
+  });
+
+  describe('type overrides', () => {
+    it('will not apply overridden types', () => {
+      const messages = [{message:'a message for you', type: 'bad'}];
+      const component = render(<UserMessages messages={ messages } typeOverride={'good'}/>);
+
+      expect( component.find('.user-message').attr('class').split(' ') ).not.toContain('bad');
+    });
+    it('will apply new type', () => {
+      const messages = [{message:'a message for you', type: 'bad'}];
+      const component = render(<UserMessages messages={ messages } typeOverride={'good'}/>);
+
+      expect( component.find('.user-message').attr('class').split(' ') ).toContain('good');
+    });
+  });
 
   // The htmlSafe option has been removed until it is necessary.
   // it('displays messages in raw html', () => {
@@ -18,10 +40,4 @@ describe('UserMessages', () => {
   //   expect(component.find('Message').text()).toContain('a message for you');
   // });
 
-  // it('allows classes to be set for messages', () => {
-  //   const messages = [{message:'a message for you', type:'biggest-message'}];
-  //   const component = mount(<UserMessages messages={ messages }/>);
-
-  //   expect(component.find('.biggest-message').length).toEqual(1);
-  // });
-})
+});
