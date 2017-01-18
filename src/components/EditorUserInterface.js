0edbac3880513b24ec1e8cf07b199b78a46741bf
Don Denton <don@happycollision.com>
2017-01-18 09:47:50 -0600

    move images up top level
    also render some images

--- src/components/EditorUserInterface.js
+++ src/components/EditorUserInterface.js
@@ -18,7 +18,11 @@ var EditorUserInterface = React.createClass({
     return {
       allowSubmit: false,
       textFields: {},
-      caughtErrors: []
+      caughtErrors: [],
+      userImages: [
+        {url: 'https://verydandy.files.wordpress.com/2012/02/robot-head.png?w=580', id:1},
+        {url: 'http://southoldlibrary.org/wp-content/uploads/2016/12/Robots.jpg', id:2}
+      ]
     };
   },
 
@@ -34,7 +38,7 @@ var EditorUserInterface = React.createClass({
     var stateToMerge = clone(startingPoint);
 
     stateToMerge.defaultResolutionId = startingPoint.resolutionId;
-    
+
     if (startingPoint.nameValuePairs !== undefined) {
       var dataTypeContainers = ['textFields', 'dropDowns', 'textBoxes', 'colorPickers', 'youTubeLinks'];
       var confirmedContainers = [];
@@ -44,7 +48,7 @@ var EditorUserInterface = React.createClass({
           confirmedContainers.push(dataTypeContainers[i]);
         }
       }
-      
+
       for (var i = 0; i < startingPoint.nameValuePairs.length; i++) {
         var name = startingPoint.nameValuePairs[i].name;
         var value = startingPoint.nameValuePairs[i].value;
@@ -59,13 +63,13 @@ var EditorUserInterface = React.createClass({
         }
       }
     }
-    
+
     // done with this now
     delete stateToMerge.nameValuePairs;
-    
+
     this.setState(stateToMerge);
   },
-  
+
   // Returns true if it passes, or an array of strings describing
   // why it didn't pass.
   checkResult: function (results) {
@@ -75,14 +79,14 @@ var EditorUserInterface = React.createClass({
     if (results.templateId.toString() !== userSettingsData.templateId.toString()) {
       messages.push(`Template IDs do not match. This page reports: ${userSettingsData.templateId}, JSON file reports: ${results.templateId}`);
     }
-    
+
     if (messages.length === 0){
       return true;
     } else {
       return messages;
     }
   },
-  
+
   tryJsonFile: function () { return new Promise((resolve,reject) => {
     this.serverRequest = getJSON(this.props.uiSettingsJsonUrl)
     .then( result => {
@@ -107,7 +111,7 @@ var EditorUserInterface = React.createClass({
       reject(`${error.name}: ${error.message}`);
     });
   })},
-  
+
   componentDidMount: function () {
     this.tryJsonFile()
     .catch((possibleReason)=>{
@@ -119,11 +123,11 @@ var EditorUserInterface = React.createClass({
       });
     });
   },
-  
+
   componentWillUnmount: function () {
     this.serverRequest.abort();
   },
-  
+
   handleFieldsChange: function (fieldName, userText) {
     var fields = this.state.textFields;
     fields[fieldName].value = userText;
@@ -160,7 +164,7 @@ var EditorUserInterface = React.createClass({
     youTubeLinksState[fieldName].title = title;
     this.setState({youTubeLinks: youTubeLinksState});
   },
-  
+
   handleResolutionIdChange: function (id) {
     this.setState({
       resolutionId: id
@@ -173,8 +177,8 @@ var EditorUserInterface = React.createClass({
     })
   },
 
-  handleUpdateImages: function () {
-    return;
+  handleUpdateImages: function (newArray) {
+    this.setState({userImages: newArray});
   },
 
   transformYouTubeLinkData: function (linkObj) {
@@ -194,13 +198,13 @@ var EditorUserInterface = React.createClass({
 
   handlePlaceOrder: function () {
     var order = {};
-    
+
     // add necessaries
     order.ui = this.state.ui;
     order.isPreview = this.state.isPreview;
     order.audioInfo = this.state.audioInfo;
     order.resolutionId = this.state.resolutionId;
-    
+
     // add values to order.ui
     for (var i = 0; i < order.ui.length; i++) {
       for (var key in order.ui[i]) {
@@ -208,11 +212,11 @@ var EditorUserInterface = React.createClass({
           for (var j = 0; j < order.ui[i][key].length; j++) {
             var name = order.ui[i][key][j].name;
             var type = order.ui[i][key][j].type;
-            
+
             // convert things like 'TextField' to 'textFields'
             type = type.charAt(0).toLowerCase() + type.slice(1) + 's';
             type = (type == 'textBoxs') ? 'textBoxes' : type; // TODO: fix this hack
-            
+
             if (type === 'youTubeLinks') {
               order.ui[i][key][j].value = this.transformYouTubeLinkData(clone(this.state[type][name]));
             } else {
@@ -222,9 +226,9 @@ var EditorUserInterface = React.createClass({
         }
       }
     }
-    
+
     var orderPromise = new Promise((resolve,reject) => {
-    
+
     xmlParser.updateXmlForOrder(order)
       .done(function(){
         resolve()
@@ -233,7 +237,7 @@ var EditorUserInterface = React.createClass({
         reject(failureReason)
       })
     });
-    
+
     orderPromise.then(function(){
       this.setState({allowSubmit: true}, function () {
         setTimeout(function(){ find('form input[type="submit"]').eq(0).click(); }, 100);
@@ -248,7 +252,7 @@ var EditorUserInterface = React.createClass({
           {message: message}
         ]
       })
-      
+
       // This method of calling console (essentially) tells the build
       // script that this is an intentional call, meant for production
       var c = console;
@@ -256,11 +260,11 @@ var EditorUserInterface = React.createClass({
       c.error('Order Failure: ' + failureReason);
     }.bind(this));
   },
-  
+
   handleChooseSong: function (audioInfo) {
     this.setState({audioInfo: audioInfo})
   },
-  
+
   render: function() {
     var resolutionPicker = (<span></span>);
     if (this.state.defaultResolutionId !== undefined) {
--- src/components/ImageContainer.js
+++ src/components/ImageContainer.js
@@ -2,17 +2,12 @@ import React from 'react';
 import DraggableList from 'react-draggable-list';
 import './ImageContainer.scss';
 
-let testImages = [
-  {name: 'something.jpg', id:1},
-  {name: 'somethingelse.jpg', id:2}
-];
-
 let ListImage = React.createClass({
   render: function () {
     let {dragHandle, item} = this.props;
     return (
       <div className='reactBasicTemplateEditor-ImageContainer-imageListItem'>
-        <span>name: {item.name}</span>
+        <img src={ item.url } />
         {dragHandle(<span className='reactBasicTemplateEditor-ImageContainer-imageListItemDragHandle'>|||</span>)}
       </div>
     );
@@ -23,14 +18,14 @@ var ImageContainer = React.createClass({
   getInitialState: function () {
     return {modalIsOpen: false};
   },
-  
+
   _onListChange: function (newList) {
     // this.setState({list: newList});
-    testImages = newList;
+    // testImages = newList;
   },
-  
+
   render: function () {
-    let images = [].concat(testImages);
+    let images = [].concat(this.props.images);
     if (images.length === 0) return null;
     return (
       <div className="reactBasicTemplateEditor-ImageContainer">
@@ -38,7 +33,7 @@ var ImageContainer = React.createClass({
           itemKey='id'
           template={ ListImage }
           list={ images }
-          onMoveEnd={ newList => this._onListChange(newList) }
+          onMoveEnd={ newList => this.props.onUpdateImages(newList) }
           container={() => document.body}
         />
       </div>
