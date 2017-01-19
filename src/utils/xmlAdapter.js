2d73837edb754dacb6765099309fbb11fdf82407
Don Denton <don@happycollision.com>
2017-01-19 16:23:44 -0600

    remove jxb from jxon

--- src/utils/xmlAdapter.js
+++ src/utils/xmlAdapter.js
@@ -1,13 +1,7 @@
 import jxon from 'jxon';
-// import jsb from 'js-beautify';
 
 jxon.config({
   parseValues: true
 });
-
-export function prettyXML (str) {
-  // return jsb.html(str);
-  return str;
-}
 
 export default jxon;
