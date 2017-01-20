ead5386f837055ca15d693ce3d310b657062ff57
Don Denton <don@happycollision.com>
2017-01-20 10:53:58 -0600

    space removal

--- src/components/EditorUserInterface.js
+++ src/components/EditorUserInterface.js
@@ -34,7 +34,7 @@ var EditorUserInterface = React.createClass({
     var stateToMerge = clone(startingPoint);
 
     stateToMerge.defaultResolutionId = startingPoint.resolutionId;
-    
+
     if (startingPoint.nameValuePairs !== undefined) {
       var dataTypeContainers = ['textFields', 'dropDowns', 'textBoxes', 'colorPickers', 'youTubeLinks'];
       var confirmedContainers = [];
@@ -44,7 +44,7 @@ var EditorUserInterface = React.createClass({
           confirmedContainers.push(dataTypeContainers[i]);
         }
       }
-      
+
       for (var i = 0; i < startingPoint.nameValuePairs.length; i++) {
         var name = startingPoint.nameValuePairs[i].name;
         var value = startingPoint.nameValuePairs[i].value;
@@ -59,13 +59,13 @@ var EditorUserInterface = React.createClass({
         }
       }
     }
-    
+
     // done with this now
     delete stateToMerge.nameValuePairs;
-    
+
     this.setState(stateToMerge);
   },
-  
+
   // Returns true if it passes, or an array of strings describing
   // why it didn't pass.
   checkResult: function (results) {
@@ -75,14 +75,14 @@ var EditorUserInterface = React.createClass({
     if (results.templateId.toString() !== userSettingsData.templateId.toString()) {
       messages.push(`Template IDs do not match. This page reports: ${userSettingsData.templateId}, JSON file reports: ${results.templateId}`);
     }
-    
+
     if (messages.length === 0){
       return true;
     } else {
       return messages;
     }
   },
-  
+
   tryJsonFile: function () { return new Promise((resolve,reject) => {
     this.serverRequest = getJSON(this.props.uiSettingsJsonUrl)
     .then( result => {
@@ -107,7 +107,7 @@ var EditorUserInterface = React.createClass({
       reject(`${error.name}: ${error.message}`);
     });
   })},
-  
+
   componentDidMount: function () {
     this.tryJsonFile()
     .catch((possibleReason)=>{
@@ -119,11 +119,11 @@ var EditorUserInterface = React.createClass({
       });
     });
   },
-  
+
   componentWillUnmount: function () {
     this.serverRequest.abort();
   },
-  
+
   handleFieldsChange: function (fieldName, userText) {
     var fields = this.state.textFields;
     fields[fieldName].value = userText;
@@ -160,7 +160,7 @@ var EditorUserInterface = React.createClass({
     youTubeLinksState[fieldName].title = title;
     this.setState({youTubeLinks: youTubeLinksState});
   },
-  
+
   handleResolutionIdChange: function (id) {
     this.setState({
       resolutionId: id
@@ -190,13 +190,13 @@ var EditorUserInterface = React.createClass({
 
   handlePlaceOrder: function () {
     var order = {};
-    
+
     // add necessaries
     order.ui = this.state.ui;
     order.isPreview = this.state.isPreview;
     order.audioInfo = this.state.audioInfo;
     order.resolutionId = this.state.resolutionId;
-    
+
     // add values to order.ui
     for (var i = 0; i < order.ui.length; i++) {
       for (var key in order.ui[i]) {
@@ -204,11 +204,11 @@ var EditorUserInterface = React.createClass({
           for (var j = 0; j < order.ui[i][key].length; j++) {
             var name = order.ui[i][key][j].name;
             var type = order.ui[i][key][j].type;
-            
+
             // convert things like 'TextField' to 'textFields'
             type = type.charAt(0).toLowerCase() + type.slice(1) + 's';
             type = (type == 'textBoxs') ? 'textBoxes' : type; // TODO: fix this hack
-            
+
             if (type === 'youTubeLinks') {
               order.ui[i][key][j].value = this.transformYouTubeLinkData(clone(this.state[type][name]));
             } else {
@@ -218,9 +218,9 @@ var EditorUserInterface = React.createClass({
         }
       }
     }
-    
+
     var orderPromise = new Promise((resolve,reject) => {
-    
+
     xmlParser.updateXmlForOrder(order)
       .done(function(){
         resolve()
@@ -229,7 +229,7 @@ var EditorUserInterface = React.createClass({
         reject(failureReason)
       })
     });
-    
+
     orderPromise.then(function(){
       this.setState({allowSubmit: true}, function () {
         setTimeout(function(){ find('form input[type="submit"]').eq(0).click(); }, 100);
@@ -244,7 +244,7 @@ var EditorUserInterface = React.createClass({
           {message: message}
         ]
       })
-      
+
       // This method of calling console (essentially) tells the build
       // script that this is an intentional call, meant for production
       var c = console;
@@ -252,11 +252,11 @@ var EditorUserInterface = React.createClass({
       c.error('Order Failure: ' + failureReason);
     }.bind(this));
   },
-  
+
   handleChooseSong: function (audioInfo) {
     this.setState({audioInfo: audioInfo})
   },
-  
+
   render: function() {
     var resolutionPicker = (<span></span>);
     if (this.state.defaultResolutionId !== undefined) {
