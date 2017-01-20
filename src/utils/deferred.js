2a2f9704e78c3b3df591c433ab81ce5ff8de8b4f
Don Denton <don@happycollision.com>
2017-01-20 10:52:53 -0600

    chainable deferred

--- src/utils/deferred.js
+++ src/utils/deferred.js
@@ -1,18 +1,19 @@
 import '../../cfg/polyfills';
 export default function MyDeferred () {
-    var _resolve,
-        _reject,
-        capturedPromise = new Promise(function(resolve, reject){
-            _resolve = resolve;
-            _reject  = reject;
-        });
+  var _resolve,
+  _reject,
+  capturedPromise = new Promise(function(resolve, reject){
+    _resolve = resolve;
+    _reject  = reject;
+  });
 
-    return {
-        'resolve' : _resolve,
-        'reject'  : _reject,
-        'then'    : function() { capturedPromise.then.apply(capturedPromise, arguments); },
-        'done'    : function() { capturedPromise.then.apply(capturedPromise, arguments); },
-        'catch'   : function() { capturedPromise.catch.apply(capturedPromise, arguments); },
-        'fail'   : function() { capturedPromise.catch.apply(capturedPromise, arguments); }
-    }
+  var returnObj = {
+    'resolve' : _resolve,
+    'reject'  : _reject,
+    'then'    : function() { capturedPromise.then.apply(capturedPromise, arguments); return returnObj; },
+    'done'    : function() { capturedPromise.then.apply(capturedPromise, arguments); return returnObj; },
+    'catch'   : function() { capturedPromise.catch.apply(capturedPromise, arguments); return returnObj; },
+    'fail'    : function() { capturedPromise.catch.apply(capturedPromise, arguments); return returnObj; }
+  };
+  return returnObj;
 };
