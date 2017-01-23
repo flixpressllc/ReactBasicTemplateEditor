6e8ab533c212bce0b2ab7d453986ce03840f163b
Don Denton <don@happycollision.com>
2017-01-23 12:05:02 -0600

    adds an attempted feature spec. need to mock $

---
+++ specs/feature-images-only-display-in-image-templates.spec.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { mount, render } from 'enzyme';
+import EditorUserInterface from '../src/components/EditorUserInterface';
+
+// need to really mock out lost of jquery stuff
+xdescribe('Feature: Images only display in image templates', () => {
+  beforeAll( () => { global.window.$ = () => {}; } )
+  it('renders images', () => {
+    let settings = {
+      uiSettingsJsonUrl: '/templates/Template1000.json',
+      templateType: 'images',
+      userSettingsData: {
+        username: 'DonDenton',
+        templateId: 79,
+        minutesRemainingInContract: 170.2819,
+        minimumTemplateDuration: 0.1667,
+        previewVideoUrl: '',
+        isChargePerOrder: false,
+        renderCost: 8
+      }
+    };
+
+    const component = mount(<EditorUserInterface
+      templateType={ settings.templateType }
+      uiSettingsJsonUrl={ settings.uiSettingsJsonUrl }
+      userSettingsData={ settings.userSettingsData }/>);
+
+    expect(component.find('ImageContainer').length).toEqual(1);
+  });
+});
