0335b381186bf8dd6d9bfaccde058144174dfa40
Don Denton <don@happycollision.com>
2017-02-02 10:51:19 -0600

    write test for images

--- src/components/ImageContainer.spec.js
+++ src/components/ImageContainer.spec.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import { mount, render, shallow } from 'enzyme';
-import ImageContainer from './ImageContainer'
+import ImageContainer from './ImageContainer';
+import { isObject } from '../utils/helper-functions';
 
 // <ImageContainer
-//   images={this.state.audioInfo}
-//   onUpdateImages={this.handleChooseSong}
+//   images={ [ arrayOfImages ] }
+//   onUpdateImages={ () => {} }
 // />
+
+function getSettings ( overrides ) {
+  overrides = isObject(overrides) ? overrides : {};
+  let defaults = {
+    images: [],
+    onUpdateImages: () => {},
+    imageBank: []
+  };
+  return Object.assign(defaults, overrides);
+}
 
 describe('ImageContainer', () => {
   it('renders without crashing', () => {
     expect(() => {
-      mount(<ImageContainer images={ [] }  onUpdateImages={ jest.fn() }/>);
+      mount(<ImageContainer {...getSettings()}/>);
     }).not.toThrow();
+  });
+
+  it('displays images', () => {
+    let settings = {
+      images: [
+        {file: 'toast.jpg', id: 0},
+        {file: 'coffee.jpg', id: 1}
+      ]
+    };
+    let component = mount(<ImageContainer {...getSettings(settings)}/>);
+    expect(component.find('img').length).toEqual(2);
   });
 });
