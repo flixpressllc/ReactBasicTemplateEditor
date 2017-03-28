4ced49d0846b28c1163b0aaa1943b44ea3b9d261
Don Denton <don@happycollision.com>
2017-03-28 16:11:30 -0500

    expand gulpfile for releasing

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,16 @@ const rename = require('gulp-rename');
 const vp = require('vinyl-paths');
 const del = require('del');
 const gReplace = require('gulp-replace');
-const rs = require('run-sequence')
+const rs = require('run-sequence');
+const bump = require('gulp-bump');
+const argv = require('yargs').argv;
+const git = require('gulp-git');
+const fs = require('fs');
+const semver = require('semver');
+
+function getPackageJson () {
+  return JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+}
 
 // See the .example.env file to learn how to set up your required .env file.
 require('dotenv').load();
@@ -28,3 +37,29 @@ gulp.task('aws', () => {
   return gulp.src('dist/**')
     .pipe( s3(awsCredentials, awsOptions) );
 });
+
+
+
+let increment, currentVersion, releaseVersion, continuingVersion;
+
+gulp.task('release', () => {
+  increment = argv.increment || 'patch';
+  currentVersion = getPackageJson().version;
+  releaseVersion = semver.inc(currentVersion, increment);
+  continuingVersion = semver.inc(releaseVersion, 'patch') + '-pre';
+  console.log(increment, currentVersion, releaseVersion, continuingVersion);
+
+  rs('bumpToRelease', 'commitAllForRelease')
+});
+
+gulp.task('bumpToRelease', () => {
+  return gulp.src('./package.json')
+  .pipe(bump({version: releaseVersion}))
+  .pipe(gulp.dest('./'));
+});
+
+gulp.task('commitAllForRelease', () => {
+  return gulp.src('./*')
+    .pipe(git.add())
+    .pipe(git.commit('Release Version v' + releaseVersion))
+})
