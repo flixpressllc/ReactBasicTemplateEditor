366536c9fcb265634d79d9c611b5ad98f681546d
Don Denton <don@happycollision.com>
2017-03-28 19:58:58 -0500

    remove flix-dev

--- cfg/flix-dev.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-let buildConfig = require('./common/build');
-
-let config = Object.assign({}, buildConfig, {
-  plugins: require('./common/plugins').devPlugins,
-});
-
-module.exports = config;
