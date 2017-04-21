b3cf186ddd620b887a80331c1c2eda146f9036ef
Don Denton <don@happycollision.com>
2017-04-21 16:10:07 -0500

    Refactor get audio options for testing (#62)
    * refactor 1
    
    * extract create cat obj
    
    * refactor createCategoriesObjForUser and arrow functions
    
    * missed a done/then thing
    
    * reafctor default export
    
    * remove unnecessary arg from function
    
    * remove unused function
    
    * function to es6
    
    * catch everything?
    
    * passing tests that depend on live data
    
    * fix bug where one custom audio won't show
    
    * update changelog

--- src/utils/getAudioOptions.js
+++ src/utils/getAudioOptions.js
@@ -1,80 +1,93 @@
 import {nativeXmlToObject} from './xmlAdapter';
+import { forceArray } from 'happy-helpers';
 
-var getCatSongs = function (categoryId, username) {
-  var audioUrl = 'https://ws.flixpress.com/AudioWebService.asmx/GetAudio';
-  return $.ajax({
-    url: audioUrl,
-    data: {username: username, categoryId: categoryId, page:1, pageSize: 1000},
-    dataType: 'xml',
-    type: 'GET'
-  });
+// https://ws.flixpress.com/AudioWebService.asmx/GetAudio?categoryId=2&page=1&pageSize=100&username=bowdo
 
+// https://ws.flixpress.com/AudioWebService.asmx/GetCategoryTree
+
+const CATEGORY_URL = 'https://ws.flixpress.com/AudioWebService.asmx/GetCategoryTree';
+const CUSTOM_URL = 'https://ws.flixpress.com/CustomAudioWebService.asmx/GetCustomAudio';
+const AUDIO_URL = 'https://ws.flixpress.com/AudioWebService.asmx/GetAudio';
+
+export function _createCategoriesObjForUser (categories, username) {
+  return new Promise((res,rej) => {
+    var categoriesObj = {};
+
+    Promise.all(
+      categories.map(category => {
+        return _getSongsFromCategoryForUser(category.Id, username)
+        .then(songs => {
+          categoriesObj[category.Name] = {};
+          categoriesObj[category.Name].id = category.Id;
+          categoriesObj[category.Name].songs = songs;
+        }).catch(err => rej(err));
+      })
+    ).then(() => {
+      res(categoriesObj);
+    }).catch(err => rej(err));
+  }).catch(err => {throw err});
+}
+
+export function _getSongsFromCategoryForUser(categoryId, username) {
+  return new Promise((res, rej) => {
+    $.ajax({
+      url: AUDIO_URL,
+      data: {username: username, categoryId: categoryId, page:1, pageSize: 1000},
+      dataType: 'xml',
+      type: 'GET'
+    }).done(result => {
+      let songs = nativeXmlToObject(result).ResultSetOfAudio.Records.Audio;
+      res(forceArray(songs));
+    }).fail(err => rej(err));
+  }).catch(err => {throw err});
+}
+
+export function _getCustomSongsForUser (username) {
+  return new Promise((res, rej) => {
+    $.ajax({
+      url: CUSTOM_URL,
+      dataType: 'xml',
+      type: 'GET',
+      data: {username: username, page:1, pageSize: 1000}
+    }).done(result => {
+      var songs = nativeXmlToObject(result).ResultSetOfCustomAudio.Records.CustomAudio;
+      res(forceArray(songs));
+    }).fail(err => rej(err));
+  }).catch(err => {throw err});
+}
+
+export function _getAllAvailableCategories () {
+  return new Promise((res, rej) => {
+    $.ajax({
+      url: CATEGORY_URL,
+      dataType: 'xml',
+      type: 'GET'
+    }).done(result => {
+      res(nativeXmlToObject(result).ArrayOfCategory.Category.SubCategories.Category);
+    }).fail(err => rej(err));
+  }).catch(err => {throw err});
+}
+
+export function _getCategoriesAndSongsForUser (username) {
+  return new Promise((res, rej) => {
+    _getAllAvailableCategories()
+    .then(categories => _createCategoriesObjForUser(categories, username))
+    .then(categoriesObj => res(categoriesObj))
+    .catch(err => rej(err));
+  }).catch(err => {throw err});
 }
 
 export default function getAudioOptions (username) {
-  var optionsPromise = $.Deferred();
-  // https://ws.flixpress.com/AudioWebService.asmx/GetAudio?categoryId=2&page=1&pageSize=100&username=bowdo
+  var everythingIsReady = $.Deferred();
 
-  // https://ws.flixpress.com/AudioWebService.asmx/GetCategoryTree
+  Promise.all([
+    _getCategoriesAndSongsForUser(username),
+    _getCustomSongsForUser(username)
+  ])
+  .then(([categories, customAudio]) => {
+    everythingIsReady.resolve({categories, customAudio});
+  })
+  .catch(err => everythingIsReady.reject(err));
 
-  var categoryUrl = 'https://ws.flixpress.com/AudioWebService.asmx/GetCategoryTree';
-  var customUrl = 'https://ws.flixpress.com/CustomAudioWebService.asmx/GetCustomAudio';
-
-  function whenAll(arrayOfPromises) {
-    return $.when.apply($, arrayOfPromises).then(function() {
-      return Array.prototype.slice.call(arguments, 0);
-    });
-  }
-
-  var categoriesObj = {};
-  var customAudioArr = [];
-  var haveAllCats = $.Deferred();
-  var haveAllCustom = $.Deferred();
-
-  var getCats = $.ajax({
-    url: categoryUrl,
-    dataType: 'xml',
-    type: 'GET'
-  });
-
-  getCats.done(function(result){
-    var getAllCats = [];
-    var categories = nativeXmlToObject(result).ArrayOfCategory.Category.SubCategories.Category;
-
-    $.each(categories, function(arrPos, category){
-      var catSongs = getCatSongs(category.Id, username)
-      getAllCats.push(catSongs);
-      catSongs.done(function(result){
-        categoriesObj[category.Name] = {};
-        categoriesObj[category.Name].id = category.Id;
-        categoriesObj[category.Name].songs = nativeXmlToObject(result).ResultSetOfAudio.Records.Audio;
-      });
-    });
-
-    whenAll(getAllCats).done(function(){
-      haveAllCats.resolve();
-    })
-  });
-
-  var getCustom = $.ajax({
-    url: customUrl,
-    dataType: 'xml',
-    type: 'GET',
-    data: {username: username, page:1, pageSize: 1000}
-  });
-
-  getCustom.done(function (result) {
-    var songs = nativeXmlToObject(result).ResultSetOfCustomAudio.Records.CustomAudio;
-    songs = songs === undefined ? [] : songs;
-    for (var i = 0; i < songs.length; i++) {
-      customAudioArr.push(songs[i]);
-    }
-    haveAllCustom.resolve();
-  });
-
-  $.when(haveAllCats, haveAllCustom).then(function () {
-    optionsPromise.resolve({categories:categoriesObj, customAudio: customAudioArr});
-  });
-
-  return optionsPromise;
+  return everythingIsReady;
 }
--- src/utils/getAudioOptions.spec.js
+++ src/utils/getAudioOptions.spec.js
@@ -1,10 +1,35 @@
-import * as getAudioOptions from './getAudioOptions';
+import getAudioOptions, {_getCustomSongsForUser} from './getAudioOptions';
+import { toType } from 'happy-helpers';
 
-describe('getAutioOptions', () => {
-  describe('custom audio', () => {
-    it('returns an empty array if custom audio is unavailable', () => {
-      pending();
-      getAudioOptions.getAudioOptions('DonDenton');
+global.window.$ = require('jquery');
+
+describe('_getCustomSongsForUser', () => {
+  it('returns an empty array if custom audio is unavailable', () => {
+    return _getCustomSongsForUser('devolved40').then(result => {
+      expect(result).toEqual([])
     });
+  });
+
+  it('returns an array if 1 result', () => {
+    return _getCustomSongsForUser('DonDentonAdmin').then(result => {
+      expect(toType(result)).toEqual('array')
+      expect(result.length).toEqual(1)
+    });
+  });
+});
+
+describe('getAudioOptions', () => {
+  it('returns a promise that responds to done', () => {
+    const done = getAudioOptions('bowdo').done
+    expect(done).not.toEqual(undefined);
+  });
+
+  it('yields the expected data in done', () => {
+    const p = new Promise(res => {
+      getAudioOptions('DonDenton').done(data => res(data))
+    });
+    return p.then(results => {
+      expect(results).toMatchSnapshot();
+    })
   });
 });
