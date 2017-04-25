0f7a840d756e9595d1be4f5400d28dc143d1bc86
Don Denton <don@happycollision.com>
2017-04-25 17:23:05 -0500

    proper ajax in getAudioOptions

--- src/utils/getAudioOptions.js
+++ src/utils/getAudioOptions.js
@@ -1,5 +1,6 @@
 import {nativeXmlToObject} from './xmlAdapter';
 import { forceArray } from 'happy-helpers';
+import { ajax } from './ajax';
 
 // https://ws.flixpress.com/AudioWebService.asmx/GetAudio?categoryId=2&page=1&pageSize=100&username=bowdo
 
@@ -30,41 +31,41 @@ export function _createCategoriesObjForUser (categories, username) {
 
 export function _getSongsFromCategoryForUser(categoryId, username) {
   return new Promise((res, rej) => {
-    $.ajax({
+    ajax({
       url: AUDIO_URL,
       data: {username: username, categoryId: categoryId, page:1, pageSize: 1000},
       dataType: 'xml',
       type: 'GET'
-    }).done(result => {
-      let songs = nativeXmlToObject(result).ResultSetOfAudio.Records.Audio;
+    }).then(result => {
+      let songs = nativeXmlToObject(result.data).ResultSetOfAudio.Records.Audio;
       res(forceArray(songs));
-    }).fail(err => rej(err));
+    }).catch(err => rej(err));
   }).catch(err => {throw err});
 }
 
 export function _getCustomSongsForUser (username) {
   return new Promise((res, rej) => {
-    $.ajax({
+    ajax({
       url: CUSTOM_URL,
       dataType: 'xml',
       type: 'GET',
       data: {username: username, page:1, pageSize: 1000}
-    }).done(result => {
-      var songs = nativeXmlToObject(result).ResultSetOfCustomAudio.Records.CustomAudio;
+    }).then(result => {
+      var songs = nativeXmlToObject(result.data).ResultSetOfCustomAudio.Records.CustomAudio;
       res(forceArray(songs));
-    }).fail(err => rej(err));
+    }).catch(err => rej(err));
   }).catch(err => {throw err});
 }
 
 export function _getAllAvailableCategories () {
   return new Promise((res, rej) => {
-    $.ajax({
+    ajax({
       url: CATEGORY_URL,
       dataType: 'xml',
       type: 'GET'
-    }).done(result => {
-      res(nativeXmlToObject(result).ArrayOfCategory.Category.SubCategories.Category);
-    }).fail(err => rej(err));
+    }).then(result => {
+      res(nativeXmlToObject(result.data).ArrayOfCategory.Category.SubCategories.Category);
+    }).catch(err => rej(err));
   }).catch(err => {throw err});
 }
 
