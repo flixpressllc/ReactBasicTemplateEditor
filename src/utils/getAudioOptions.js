d6e852a999eacacc7f5249139b8d031a7b1c47f1
Don Denton <don@happycollision.com>
2017-04-27 01:58:36 -0500

    remove jQuery-based ajax method
    now using fetch with polyfill

--- src/utils/getAudioOptions.js
+++ src/utils/getAudioOptions.js
@@ -1,6 +1,6 @@
-import {nativeXmlToObject} from './xmlAdapter';
+import { xmlStringToObject } from './xmlAdapter';
 import { forceArray } from 'happy-helpers';
-import { ajax } from './ajax';
+import { fetch as ffetch, fetchWithParams } from './ajax';
 
 // https://ws.flixpress.com/AudioWebService.asmx/GetAudio?categoryId=2&page=1&pageSize=100&username=bowdo
 
@@ -31,13 +31,14 @@ export function _createCategoriesObjForUser (categories, username) {
 
 export function _getSongsFromCategoryForUser(categoryId, username) {
   return new Promise((res, rej) => {
-    ajax({
-      url: AUDIO_URL,
-      data: {username: username, categoryId: categoryId, page:1, pageSize: 1000},
-      dataType: 'xml',
-      type: 'GET'
-    }).then(result => {
-      let songs = nativeXmlToObject(result.data).ResultSetOfAudio.Records.Audio;
+    const dataToSend = {
+      username: username,
+      categoryId: categoryId,
+      page: 1,
+      pageSize: 1000
+    };
+    fetchWithParams(AUDIO_URL, dataToSend).then(response => response.text()).then(result => {
+      let songs = xmlStringToObject(result).ResultSetOfAudio.Records.Audio;
       res(forceArray(songs));
     }).catch(err => rej(err));
   }).catch(err => {throw err});
@@ -45,13 +46,13 @@ export function _getSongsFromCategoryForUser(categoryId, username) {
 
 export function _getCustomSongsForUser (username) {
   return new Promise((res, rej) => {
-    ajax({
-      url: CUSTOM_URL,
-      dataType: 'xml',
-      type: 'GET',
-      data: {username: username, page:1, pageSize: 1000}
-    }).then(result => {
-      var songs = nativeXmlToObject(result.data).ResultSetOfCustomAudio.Records.CustomAudio;
+    const dataToSend = {
+      username: username,
+      page: 1,
+      pageSize: 1000
+    };
+    fetchWithParams(CUSTOM_URL, dataToSend).then(response => response.text()).then(result => {
+      var songs = xmlStringToObject(result).ResultSetOfCustomAudio.Records.CustomAudio;
       res(forceArray(songs));
     }).catch(err => rej(err));
   }).catch(err => {throw err});
@@ -59,12 +60,8 @@ export function _getCustomSongsForUser (username) {
 
 export function _getAllAvailableCategories () {
   return new Promise((res, rej) => {
-    ajax({
-      url: CATEGORY_URL,
-      dataType: 'xml',
-      type: 'GET'
-    }).then(result => {
-      res(nativeXmlToObject(result.data).ArrayOfCategory.Category.SubCategories.Category);
+    ffetch(CATEGORY_URL).then(response => response.text()).then(result => {
+      res(xmlStringToObject(result).ArrayOfCategory.Category.SubCategories.Category);
     }).catch(err => rej(err));
   }).catch(err => {throw err});
 }
