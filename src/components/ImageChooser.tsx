faf8cd6c50ec4064e2eae578aa3e46ee4c5d0d2e
Don Denton <don@happycollision.com>
2017-08-15 18:22:07 -0500

    add placeholder functionality

--- src/components/ImageChooser.tsx
+++ src/components/ImageChooser.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import * as React from 'react';
 import { THUMBNAIL_URL_PREFIX } from '../stores/app-settings';
 import './ImageChooser.scss';
 
-const ImageChooser = (props) => {
+interface P {
+  onChooseImage(imageUrl: string): void,
+  imageBank: Array<string>
+}
+
+function invokeImageUploader() {
+  // nothing for now.
+}
+
+const ImageChooser = (props: P) => {
   let imageList = props.imageBank.map((imageUrl, i) => {
     return (<img src={ THUMBNAIL_URL_PREFIX + imageUrl } key={ i } onClick={ () => { props.onChooseImage(imageUrl) }}/>);
   });
   return (
     <div className="reactBasicTemplateEditor-ImageContainer-imageBank">
       <p className="reactBasicTemplateEditor-ImageContainer-imageBankInstructions">
-        <button>Upload a new image</button> or select from below:
+        <button onClick={invokeImageUploader}>Upload a new image</button> or select from below:
       </p>
       { imageList }
     </div>
