e67fbed02c0cfa1f0e5f47f197a725ea1ffe9f60
Don Denton <don@happycollision.com>
2017-08-21 16:59:57 -0500

    Initial import of cropping library

--- types/typings.d.ts
+++ types/typings.d.ts
@@ -1,5 +1,22 @@
 type FileUploadData = any;
 type FileUploadResponse = any;
+
 interface FileChangeEvent extends Event {
   target: Event['target'] & {files: File[]}
+}
+
+// Fix for bad TS typing for FileReader types
+interface FileReaderEventTarget extends EventTarget {
+  result:string
+}
+
+interface FileReaderEvent extends Event {
+  target: FileReaderEventTarget;
+  getMessage():string;
+}
+// end fix
+
+interface EmittedEvent {
+  data: any;
+  type: string;
 }
